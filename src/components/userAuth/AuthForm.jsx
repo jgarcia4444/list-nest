f14c4e1d75af1b88fccbf68254f1d1bada2835e7
Jake Garcia <bballwiz4@gmail.com>
2024-07-17 10:54:23 -0600

    Changed the render inputs method to filter and show the email and password for login instead of the first name and the last name.

--- src/components/userAuth/AuthForm.jsx
+++ src/components/userAuth/AuthForm.jsx
@@ -6,14 +6,16 @@ import FormInput from './FormInput/FormInput';
 
 import formChange from '../../redux/actions/authActions/formChange';
 
-const AuthForm = ({login}) => {
+const AuthForm = ({login, authInfo}) => {
 
     const iconSize = 24;
     const iconColor = "#f5ebe7";
 
+    const {firstName, lastName} = authInfo;
+
     const formInputs = [
-        {label: "First Name", inputType: "text", icon: <FiUser size={iconSize} color={iconColor} />, placeholder: "John", value: "", onChange: () => formChange({firstName})},
-        {label: "Last Name", inputType: "text", icon: <FiUser size={iconSize} color={iconColor} />, placeholder: "Doe", value: "", onChange: ""},
+        {label: "First Name", inputType: "text", icon: <FiUser size={iconSize} color={iconColor} />, placeholder: "John", value: {firstName}, onChange: () => formChange({firstName})},
+        {label: "Last Name", inputType: "text", icon: <FiUser size={iconSize} color={iconColor} />, placeholder: "Doe", value: lastName, onChange: ""},
         {label: "Username", inputType: "text", icon: <FiUser size={iconSize} color={iconColor} />, placeholder: "jDoe1234", value: "", onChange: ""},
         {label: "Email", inputType: "email", icon: <FiMail size={iconSize} color={iconColor} />, placeholder: "johndoe@email.com", value: "", onChange: ""},
         {label: "Phone Number", inputType: "tel", icon: <FiPhone size={iconSize} color={iconColor} />, placeholder: "1234567891", value: "", onChange: ""},
@@ -22,14 +24,17 @@ const AuthForm = ({login}) => {
     ];
 
     const renderInputs = () => {
-        let inputRange = login === true ? 2 : formInputs.length;
-        return formInputs.slice(0, inputRange).map((info, i) => {
+        let inputs = formInputs;
+        if (login === true) {
+            inputs = formInputs.filter(inputInfo => inputInfo.label === "Email" || inputInfo.label === "Password");
+        }
+        return inputs.map((info, i) => {
             return <FormInput info={info} key={`${i}-${info.label}`} />
         })
     }
 
     return (
-        <div className="flex flex-row flex-wrap w-1/2 border-2 border-primary-green rounded items-center justify-center md:justify-between p-1">
+        <div className="flex flex-row flex-wrap w-1/2 bg-primary-green rounded items-center justify-center md:justify-between p-1">
             {renderInputs()}
         </div>
     )
--- src/components/userAuth/FormInput/FormInput.jsx
+++ src/components/userAuth/FormInput/FormInput.jsx
@@ -6,10 +6,10 @@ const FormInput = ({info}) => {
 
     return (
         <div className="flex flex-col items-start">
-            <label htmlFor={label} className="font-thin text-sm">{label}</label>
-            <div className="flex flex-row bg-primary-green rounded py-1 px-2">
+            <label className="font-thin text-sm text-nude-color">{label}</label>
+            <div className="flex flex-row bg-white bg-opacity-20 rounded py-1 px-2">
                 {icon}
-                <input name={label} placeholder={placeholder} className="ml-1 text-nude-color bg-transparent placeholder-nude-color placeholder-opacity-50" type={inputType} />
+                <input placeholder={placeholder} className="text-nude-color bg-transparent placeholder-nude-color placeholder-opacity-50 pl-2" type={inputType} />
             </div>
         </div>
     )
