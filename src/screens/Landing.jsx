063b45af622f6e4071eb56ff76ee852b19b44268
Jake Garcia <bballwiz4@gmail.com>
2024-07-26 20:28:45 -0600

    Created a function called checkLogin in the Landing component that uses firebase/auth getAuth().currentUser property to check if a user is logged in.

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { getFirestore, connectFirestoreEmulator, } from 'firebase/firestore';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getAuth, connectAuthEmulator, onAuthStateChanged } from 'firebase/auth';
 
 import logo from '../media/logo/ListNest-no-bg.png';
 
@@ -10,19 +10,37 @@ import emulators from '../config/firebaseConfiguration';
 
 const Landing = () => {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const {store, auth} = emulators;
 
+    const checkLogIn = () => {
+        let user = auth.currentUser
+        if (user) {
+            setIsLoggedIn(true)
+        } else {
+            setIsLoggedIn(false);
+        }
+    }
+
     useEffect(() => {
-        console.log("Firestore App", store);
-        console.log("Auth connection", auth);
+        checkLogIn()
     })
 
     return (
         <div className="flex flex-col w-full h-full items-center justify-center">
-            <div className="">
-                <img src={logo} alt="" className="w-32" />
-            </div>
-            <UserAuth />
+            {isLoggedIn === true ?
+                <Home />
+            :
+            (
+                <>
+                    <div className="">
+                        <img src={logo} alt="" className="w-32" />
+                    </div>
+                    <UserAuth />
+                </>
+            )
+            }
         </div>
     )
 }
