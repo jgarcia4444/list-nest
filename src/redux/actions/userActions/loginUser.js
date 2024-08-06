6ca021efa323f1d0c1611ece4c6b10ef512ec79d
Jake Garcia <bballwiz4@gmail.com>
2024-08-06 17:24:06 -0600

    Created a login user action that uses the firebase method signInWithEmailAndPassword

--- src/redux/actions/userActions/loginUser.js
+++ src/redux/actions/userActions/loginUser.js
@@ -1,7 +1,20 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import emulators from '../../../config/firebaseConfiguration'
 const loginUser = authInfo => {
-    return {
-        type: "USER_LOGGIN_IN",
-        authInfo
+
+    const {auth} = emulators;
+
+    const {email, password} = authInfo;
+
+    return async dispatch => {
+        dispatch({type: "LOGGING_IN"});
+        signInWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                return dispatch({type: "USER_LOGIN_SUCCESS"});
+            })
+            .catch(error => {
+                console.log("There was an error logging in", error.message);
+            });
     }
 }
 
