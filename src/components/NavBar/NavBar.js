08aa595ef86eae3759a9dab8cccc3e62358f1bbc
Jake Garcia <bballwiz4@gmail.com>
2024-12-26 12:55:48 -0700

    Sized the logo and created the rendernavlinks method also created the navlink component.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FiUsers } from "react-icons/fi";
+
+import logo from '../../media/logo/ListNest-no-bg.png';
+import NavLink from './NavLink/NavLink';
 
 const NavBar = () => {
 
+    const renderNavLink = () => {
+        const navLinks = [
+            {label: "Housemates", to: "housemates", icon: "", dropdownLinks: []},
+            {label: "", to: "", icon: "", dropdownLinks: []},
+        ]
+        return navLinks.map((linkInfo) => <NavLink linkInfo={linkInfo}/>)
+    }
+
     return (
-        <div className="flex flex-row">
-            Navbar
-            {/* logo */}
-            {/* navlinks */}
+        <div className="flex flex-row w-full">
+            <div className="w-1/5">
+                <img src={logo} alt="" className="w-1/2" />
+            </div>
+            <div className="w-4/5 flex flex-row items-end">
+                {renderNavLink()}
+            </div>
         </div>
     )
 }
---
+++ src/components/NavBar/NavLink/NavLink.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavLink = ({linkInfo}) => {
+
+    const {to, label,  icon, dropdownLinks} = linkInfo;
+
+    return (
+        <a href="" className="">
+            <div className="flex flex-row">
+                <div className="">
+                    
+                </div>
+                <div className="">
+
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default NavLink;
