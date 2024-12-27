dbd5da081033975b02707caf559c502c988672f5
Jake Garcia <bballwiz4@gmail.com>
2024-12-27 09:48:06 -0700

    Styled and layed out the nav bar nav links, added in the dropdownlinks rendering and created variable components to use for the drop down links. gave them styling and a minimal animation that triggers on hover. No functionality added yet.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { FiUsers } from "react-icons/fi";
+import { FiUsers, FiUser, FiSettings, FiLogOut } from "react-icons/fi";
 
 import logo from '../../media/logo/ListNest-no-bg.png';
 import NavLink from './NavLink/NavLink';
 
 const NavBar = () => {
 
+    
+
     const renderNavLink = () => {
+        const iconSize = 20;
+        const iconColor = "#89dbab";
+        
+        const settings = () => {
+            const linkInfo = {label: "Settings", to: "users/settings", icon: <FiSettings size={iconSize} color={iconColor} />}
+            return <NavLink linkInfo={linkInfo}/>
+        }
+
+        const logoutUser = () => {
+            return (
+                <div className="flex flex-row bg-white bg-opacity-40 p-1 w-full hover:bg-opacity-80 transition-all duration-300">
+                    <div className="">
+                        <FiLogOut color={iconColor} size={iconSize} />
+                    </div>
+                    <div className="">Logout</div>
+                </div>
+            )
+        }
+
         const navLinks = [
-            {label: "Housemates", to: "housemates", icon: "", dropdownLinks: []},
-            {label: "", to: "", icon: "", dropdownLinks: []},
+            {label: "Housemates", to: "housemates", icon: <FiUsers size={iconSize} color={iconColor} />, dropdownLinks: []},
+            {label: "Account", to: "#", icon: <FiUser size={iconSize} color={iconColor} />, dropdownLinks: [settings(), logoutUser()]},
         ]
         return navLinks.map((linkInfo) => <NavLink linkInfo={linkInfo}/>)
     }
 
     return (
-        <div className="flex flex-row w-full">
+        <div className="flex flex-row w-full justify-between">
             <div className="w-1/5">
                 <img src={logo} alt="" className="w-1/2" />
             </div>
-            <div className="w-4/5 flex flex-row items-end">
+            <div className="flex flex-row items-center mr-6 gap-4">
                 {renderNavLink()}
             </div>
         </div>
--- src/components/NavBar/NavLink/NavLink.js
+++ src/components/NavBar/NavLink/NavLink.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const NavLink = ({linkInfo}) => {
 
+    const [showDropdown, setShowDropdown] = useState(false);
+
     const {to, label,  icon, dropdownLinks} = linkInfo;
 
-    return (
-        <a href="" className="">
-            <div className="flex flex-row">
-                <div className="">
-                    
-                </div>
-                <div className="">
+    const handleOnClick = () => {
+        if (to === "#") {
+            setShowDropdown(!showDropdown);
+        }
+    }
 
+    const renderDropdownLinks = () => {
+        return dropdownLinks.map(dropdownLink => dropdownLink)   
+    }
+
+    return (
+        <a href={to} onClick={handleOnClick} className="">
+            <div className={`flex flex-row relative ${label === "Settings" ? "p-1 bg-white bg-opacity-40 hover:bg-opacity-80 transition-all duration-300" : ""}`}>
+                <div className="">
+                    {icon}
                 </div>
+                <div className="">
+                    {label}
+                </div>
+                {showDropdown === true &&
+                (
+                    <div className="absolute top-6 left-0 flex flex-col items-start justify-center">
+                        {renderDropdownLinks()}
+                    </div>
+                )
+                }
             </div>
         </a>
     )
